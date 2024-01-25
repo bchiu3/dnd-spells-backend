5cfed79cbe2b0583f0bf35583d18adb48c9265fb
Brian Chiu <bhychiu89@yahoo.com>
2024-01-25 02:22:56 -0800

    Update converters.py

--- dndSpellsBackend/spells/converters.py
+++ dndSpellsBackend/spells/converters.py
@@ -6,7 +6,9 @@ class StringConverter:
     regex = "[a-zA-Z0-9 %]+"
     
     def to_python(self, value):
+        print("to_python", value)
         return urllib_parse.unquote(urllib_parse.unquote(value))
     
     def to_url(self, value):
-        return urllib_parse.unquote(urllib_parse.unquote(value))
+        print("to_url", value)
+        return value
